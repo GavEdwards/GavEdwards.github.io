53c3dfaf471850eb42d04d46a79d3e51b6a4c425
GavEdwards <gav-gav@hotmail.co.uk>
2017-10-09 21:54:47 +0100

    modified:   HackKeele/HackKeele.css 	modified:   HackKeele/HackKeele.js 	modified:   HackKeele/contact.html 	modified:   HackKeele/events.html 	modified:   HackKeele/faq.html 	copied:     HackKeele/images/background2.jpg -> HackKeele/images/background2-fullsize.jpg 	modified:   HackKeele/images/background2.jpg 	modified:   HackKeele/index.html 	modified:   HackKeele/register.html 	modified:   HackKeele/sponsers.html

--- HackKeele/HackKeele.js
+++ HackKeele/HackKeele.js
@@ -3,28 +3,21 @@
 function navClick() {
     $('#menu').toggleClass('menu-click');
 }
-
 $(document).ready(function () {
-    
     $('.pop-up-button').click(function () {
         $(this).next().toggleClass('active-pop-up');
         $('#nav-button').toggleClass('hide-nav');
-
     });
-    
-
 });
 
-//MAKE IT SO ONLY REMOVES FROM CALLED FROM OIBJECT
+
 function closePopup(pop) {
-    
-   var video = $(pop).parent().find(".pop-video").attr("src");
-        $(pop).parent().find(".pop-video").attr("src","");
-        $(pop).parent().find(".pop-video").attr("src",video);
-    
+    var video = $(pop).parent().find(".pop-video").attr("src");
+    $(pop).parent().find(".pop-video").attr("src", "");
+    $(pop).parent().find(".pop-video").attr("src", video);
     $(pop).parent().parent().parent().removeClass('active-pop-up');
     //$('.pop-up-container').removeClass('active-pop-up');
-        $('#nav-button').removeClass('hide-nav');
+    $('#nav-button').removeClass('hide-nav');
 }
 
 function arrowClick() {
